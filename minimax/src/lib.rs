5c66ae51a41bd2df56224219c0aaba9993e9a543
Jacob Gardner <jacob.v.gardner@gmail.com>
2021-04-11 16:51:36 -0500

    Tic Tac Toe

---
+++ minimax/src/lib.rs
@@ -0,0 +1,56 @@
+use std::fmt::Display;
+
+mod tictactoe;
+
+// Ultimate goal here:
+//  - I have a function that can take in a generic object of some sort Generic or Trait
+//  - That object has an iterator that can make more of itself
+//  - I can safely pass a value from the iterator back into the root function.
+
+// This seems to indicate to me two approaches:
+//  (A) Trait that create an iterator that iterates over itself
+//  (B) Generic Struct where the generic implements a trait that does the same...
+
+pub trait MinimaxNode {
+    type It: Iterator<Item = Self>;
+    fn iter(&self) -> Self::It;
+}
+
+pub fn minimax<T: MinimaxNode + Display>(
+    root_node: T,
+    max_depth: usize,
+    is_maximizing_player: bool,
+) {
+    if max_depth == 0 {
+        return;
+    }
+
+    for node in root_node.iter() {
+        minimax(node, max_depth - 1, !is_maximizing_player);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::tictactoe::{TicTacToe, TicTacToeMoveGenerator};
+
+    impl MinimaxNode for TicTacToe {
+        type It = TicTacToeMoveGenerator;
+
+        fn iter(&self) -> Self::It {
+            self.generate_moves()
+        }
+    }
+
+    #[test]
+    fn tic_tac_toe() {
+        let board = TicTacToe::new();
+
+        minimax(board, 8, true);
+
+
+        println!("The only winning move is not to play.");
+    }
+}
